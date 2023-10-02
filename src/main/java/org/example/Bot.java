0106a15b37d7cfed58d50b0898ca413428939f6c
kateromanovna <catia.gorbunova2016@yandex.ru>
2023-10-02 19:47:59 +0500

    Initial commit

---
+++ src/main/java/org/example/Bot.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        // геттер имени бота
+        return "psy_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        // геттер токена бота
+        return "6445502252:AAEhiVq9eG0DAVKfMQyUqvayLt38XoFSe-I";
+    }
+}
